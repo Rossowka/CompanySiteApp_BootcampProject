dac45f3b4d1071449d6f762461afe6b320283da7
Sara Rossow <rossowka@icloud.com>
2021-07-08 15:54:59 +0200

    created navigation component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './reset.css';
 import './App.css';
 
-import Footer from './Components/Footer'
-import LandingPage from './Components/LandingPage'
-import Main from './Components/Main'
 import Navigation from './Components/Navigation';
 
 function App() {
   return (
     <>
       <Navigation />
-      <LandingPage />
-      <Main />
-      <Footer />
     </>
   );
 }
---
+++ src/Components/Navigation.js
@@ -0,0 +1,17 @@
+const Navigation = () => {
+    return (
+        <header className="text-white">
+            <nav className="banner-split container">
+                <a href="index.html" className="bold">moja firma</a>
+                <ul id="menu_links">
+                    <li><a href="#about">o nas</a></li>
+                    <li><a href="#offer">oferta</a></li>
+                    <li><a href="#contact">kontakt</a></li>    
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+
+export default Navigation;
